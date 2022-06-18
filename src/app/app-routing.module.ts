db6cb43008cff94407c8b31059e0c5a4fa770eb6
Marcio Lima <ma4cio@gmail.com>
2022-06-18 09:36:48 -0300

    Entendendo arquivos de rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path: "", component: PageHomeComponent, pathMatch:'full'},
+  {  path: "sobre", component: PageSobreComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
